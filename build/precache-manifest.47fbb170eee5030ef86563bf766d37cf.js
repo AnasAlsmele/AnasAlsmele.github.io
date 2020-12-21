ec502ef34df4e046776a0d16d3ca4d3fcabd0d63
Anas Alsmele <52325883+AnasMusic@users.noreply.github.com>
2020-12-22 00:03:39 +0300

    build

---
+++ build/precache-manifest.47fbb170eee5030ef86563bf766d37cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95086cf8efc760f7c0bbe85237d36d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "663aa72af1c2ac9d0ccd",
+    "url": "/static/css/main.eb944fa6.chunk.css"
+  },
+  {
+    "revision": "feb1882a3f8ad215d0a1",
+    "url": "/static/js/2.2f3686d7.chunk.js"
+  },
+  {
+    "revision": "663aa72af1c2ac9d0ccd",
+    "url": "/static/js/main.ff311675.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
